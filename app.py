071ead920088a01f6e1c7c5995cfe12f04ff8aff
condog220 <103192802+condog220@users.noreply.github.com>
2025-11-20 19:37:54 +0000

    Add initial implementation of weather application using Tkinter and OpenWeatherMap API
    - Created main application window with Tkinter
    - Implemented city search functionality to fetch weather data from OpenWeatherMap API
    - Added input field and search button for user interaction
    - Integrated weather icon display using PIL for better user experience
    - Set up basic layout with grid configuration for UI elements

---
+++ app.py
@@ -0,0 +1,62 @@
+import tkinter as tk
+import requests
+from PIL import Image, ImageTk
+
+# OpenWeatherMap API configuration
+appId = "3729e98fa6f69aeccb7b10332b62f0e8"
+baseUrl = "http://api.openweathermap.org/data/2.5/weather?"
+city = ""
+completeUrl = baseUrl + "appid=" + appId + "&q=" + city + "&units=metric"
+
+root = tk.Tk()
+root.geometry("800x600")
+root.title("Weather App")
+
+searchFrame = tk.Frame(root)
+searchVar = tk.StringVar()
+
+
+def fetch_weather(city):
+    completeUrl = baseUrl + "appid=" + appId + "&q=" + city + "&units=metric"
+    response = requests.get(completeUrl)
+    weather_data = response.json()
+    print(weather_data)
+
+
+def Find():
+    city = searchVar.get()
+    fetch_weather(city)
+
+
+cityText = tk.Label(searchFrame, text= "City:")
+search_entry = tk.Entry(searchFrame, textvariable=searchVar, font=("Helvetica", 16))
+search_Button = tk.Button(searchFrame, text="Search", command=Find,  font=("Helvetica", 16))
+
+cityText.grid(row=0, column=0)
+search_entry.grid(row=0, column=1)
+search_Button.grid(row=0, column=2, padx=10)
+
+searchFrame.grid(row=0, column=1, padx=20, pady=20)
+
+### Add Weather Image
+weatherIcon = Image.open("img/weather.png")
+weatherResized = weatherIcon.resize((100, 100))
+
+
+weatherImage = ImageTk.PhotoImage(weatherResized)
+weatherLabel = tk.Label(root, image=weatherImage)
+weatherLabel.grid(row=1, column=1, padx=20, pady=20)
+
+root.columnconfigure(0, weight=1)
+root.columnconfigure(1, weight=0)
+root.columnconfigure(2, weight=1)
+
+root.mainloop()
+
+
+
+
+
+
+
+
