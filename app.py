7ff1469f56603d433bc718663b3fa48e6f90ed97
condog220 <103192802+condog220@users.noreply.github.com>
2025-11-21 14:51:43 +0000

    Updated UI and added current weather icon

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ load_dotenv()
 # OpenWeatherMap API configuration
 baseUrl = "http://api.openweathermap.org/data/2.5/weather?"
 city = ""
+colour = "#7af5f5"
 
 root = tk.Tk()
 root.geometry("800x600")
@@ -33,11 +34,12 @@ def fetch_weather():
     temperature = weather_data['main']['temp']
     humidity = weather_data['main']['humidity']
     pressure = weather_data['main']['pressure']
-
+    city = weather_data['name']
 
     tempLabel.config(text=f"Temperature: {temperature} °C")
     humidityLabel.config(text=f"Humidity: {humidity} %")
     pressureLabel.config(text=f"Pressure: {pressure} hPa")
+    currentWeatherLabel.config(text=f"Current Weather: {city}")
 #---------------------------------------------------#
 #Add Search Box
 cityText = tk.Label(searchFrame, text= "City:")
@@ -50,43 +52,45 @@ search_Button.grid(row=0, column=2, padx=10)
 
 searchFrame.grid(row=0, column=1, padx=20, pady=20)
 #---------------------------------------------------#
+#Add current weather information
+currentWeatherLabel = tk.Label(root, text="Current Weather: ", font=("Helvetica", 16), bg=colour)
+currentWeatherLabel.grid(row=0, column=2, padx=20, pady=20)
+
 ### Add Weather Details
 weatherIcon = Image.open("img/weather.png")
-weatherResized = weatherIcon.resize((100, 100))
+weatherResized = weatherIcon.resize((50, 50))
 weatherImage = ImageTk.PhotoImage(weatherResized)
 
 
-weatherLabel = tk.Label(root, image=weatherImage)
+weatherLabel = tk.Label(root, image=weatherImage, bg=colour)
 weatherLabel.grid(row=1, column=1, padx=20, pady=20)
 
-tempLabel = tk.Label(root, text="Temperature: °C", font=("Helvetica", 20))
+tempLabel = tk.Label(root, text="Temperature: °C", font=("Helvetica", 20), bg=colour)
 tempLabel.grid(row=2, column=1, padx=20, pady=10)
 #---------------------------------------------------#
 # Add Humidity Details
 humidityIcon = Image.open("img/humidity.png")
 humidityResized = humidityIcon.resize((50, 50))
 humidityImage = ImageTk.PhotoImage(humidityResized)
-humidityLabelIcon = tk.Label(root, image=humidityImage)
+humidityLabelIcon = tk.Label(root, image=humidityImage, bg=colour)
 humidityLabelIcon.place(relx=0.25, rely=0.6, anchor="center")
 
-humidityLabel = tk.Label(root, text="Humidity: %", font=("Helvetica", 20))
+humidityLabel = tk.Label(root, text="Humidity: %", font=("Helvetica", 20), bg=colour)
 humidityLabel.place(relx=0.25, rely=0.7, anchor="center")
 #---------------------------------------------------#
 #Add pressure Details
 pressureIcon = Image.open("img/pressure.png")
-pressureResized = pressureIcon.resize((100, 100))
+pressureResized = pressureIcon.resize((50, 50))
 pressureImage = ImageTk.PhotoImage(pressureResized)
-pressureLabelIcon = tk.Label(root, image=pressureImage)
+pressureLabelIcon = tk.Label(root, image=pressureImage, bg=colour)
 pressureLabelIcon.place(relx=0.75, rely=0.6, anchor="center")
-pressureLabel = tk.Label(root, text="Pressure: hPa", font=("Helvetica", 20))
+pressureLabel = tk.Label(root, text="Pressure: hPa", font=("Helvetica", 20), bg=colour)
 pressureLabel.place(relx=0.75, rely=0.7, anchor="center")
 #---------------------------------------------------#
 
 
-root.columnconfigure(0, weight=1)
-root.columnconfigure(1, weight=0)
-root.columnconfigure(2, weight=1)
 
+root.configure(bg="#7af5f5")
 root.mainloop()
 
 
